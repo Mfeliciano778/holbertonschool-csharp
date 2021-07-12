c2cb5641e8ea1c77aca54bdac0ce5eab04f467e5
Mfeliciano778 <60366104+Mfeliciano778@users.noreply.github.com>
2021-07-12 16:05:38 -0400

    Initial commit of task 9

---
+++ 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		double percent = .7553;
+		double currency = 98765.4321;
+		double sum = percent + currency;
+        Console.WriteLine("${0}", sum.ToString("0.00"));
+	}
+}
