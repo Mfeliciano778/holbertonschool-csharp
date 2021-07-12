4c532d73823d9ed86d244e4d59e59e01a819076c
Mfeliciano778 <1652@holbertonschool.com>
2021-07-12 14:08:32 -0400

    Added executable to the file

--- 0x00-csharp-hello_world/1-build_new_project.sh
+++ 0x00-csharp-hello_world/1-build_new_project.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # script that initializes, builds, and runs a new C# project from a folder titled 2-new_project
 dotnet new console -o 2-new_project
-dotnet run
+dotnet build 2-new_project
