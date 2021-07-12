1eec394c28f784532b969ac27c6310076f9a7432
Mfeliciano778 <1652@holbertonschool.com>
2021-07-12 13:19:18 -0400

    Initial commit of task 0

---
+++ 0x00-csharp-hello_world/0-initialize_new_project.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# creates a new C# project inside a folder titled 0-new_project
+
+# make and go to new directory
+mkdir 0-new_project
+cd 0-new_project
+
+# run command to make the new console app project
+dotnet new console
