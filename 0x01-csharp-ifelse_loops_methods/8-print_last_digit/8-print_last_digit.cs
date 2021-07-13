fd3a011509c471c7c5f97061c8684bb5cc64871e
Mfeliciano778 <60366104+Mfeliciano778@users.noreply.github.com>
2021-07-13 13:54:44 -0400

    Initial commit of task 8

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        Console.Write((number % 10) < 0 ? (number % 10) * -1 : number % 10);
+        return (number % 10) < 0 ? (number % 10) * -1 : number % 10;
+    }
+}
