a42b3030379d321b2731e625eeb55ea7a0c53ae6
Mfeliciano778 <60366104+Mfeliciano778@users.noreply.github.com>
2021-07-13 14:08:47 -0400

    Initial commit for task 11

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        if (length != 0)
+        {
+            for (int index = 0; index < length; index++)
+            {
+                for (int spaces = 0; spaces < index; spaces++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("\\\n");
+            }
+            Console.Write("\n");
+        } else
+            Console.Write("\n");
+    }
+}
