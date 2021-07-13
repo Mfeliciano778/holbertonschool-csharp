2171ef3c147bbd2d1617fba9d7ae7ff2e22a9b88
Mfeliciano778 <60366104+Mfeliciano778@users.noreply.github.com>
2021-07-13 11:49:14 -0400

    Initial comit of task 1

---
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10000, 10000);
+        string result;
+        if ((number % 10) > 5)
+            result = "greater than 5";
+        else if ((number % 10) == 0)
+            result = "0";
+        else
+            result = "less than 6 and not 0";
+        Console.WriteLine("The last digit of {0} is {1} and is {2}", number, number % 10, result);
+    }
+}
