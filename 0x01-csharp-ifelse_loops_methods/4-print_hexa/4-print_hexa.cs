d8c66fd755253a6a60685f051a3a956633caf301
Mfeliciano778 <60366104+Mfeliciano778@users.noreply.github.com>
2021-07-13 14:21:04 -0400

    Updated the output to match the one we want

--- 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -8,7 +8,7 @@ namespace _4_print_hexa
         {
             for (int i = 0; i <= 98; i++)
             {
-                Console.Write("{0} = {1:x}", i, i);
+                Console.Write("{0} = 0x{1:x}", i, i);
                 Console.WriteLine();
             }
         }
