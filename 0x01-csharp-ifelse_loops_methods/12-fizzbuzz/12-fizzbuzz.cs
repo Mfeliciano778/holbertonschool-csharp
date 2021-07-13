b0a3bbd432bbd6a7467ba4b8625d2e022369ba95
Mfeliciano778 <60366104+Mfeliciano778@users.noreply.github.com>
2021-07-13 14:16:33 -0400

    Initial commit of task 12

---
+++ 0x01-csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _12_fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int index = 1; index <= 100; index++)
+            {
+                if (index % 3 == 0 && index % 5 == 0)
+                {
+                    Console.Write("FizzBuzz ");
+                } else if (index % 5 == 0)
+                {
+                    Console.Write(index == 100 ? "Buzz\n" : "Buzz ");
+                } else if (index % 3 == 0)
+                {
+                    Console.Write("Fizz ");
+                } else
+                {
+                    Console.Write($"{index} ");
+                }
+            }
+        }
+    }
+}
