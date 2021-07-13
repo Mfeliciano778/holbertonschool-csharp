2a6de1a61517e61b4c017ebbec287b3a830e08c0
Mfeliciano778 <60366104+Mfeliciano778@users.noreply.github.com>
2021-07-13 14:02:33 -0400

    Intial commit of task 10

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintLine(int length)
+    {
+        if (length != 0)
+        {
+            for (int index = 0; index < length; index++)
+            {
+                Console.Write("_");
+            }
+            Console.Write("\n");
+        } else
+            Console.Write("\n");
+    }
+}
