7ee6d61b84b359277cc4183553b921a0c578a58d
Mfeliciano778 <60366104+Mfeliciano778@users.noreply.github.com>
2021-07-13 12:32:44 -0400

    Inital commit of task 6

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int tens = 0; tens <= 9; tens++)
+            {
+                for (int ones = 1; ones <= 9; ones++)
+                {
+                    if (tens == ones || ones < tens)
+                        continue;
+                    else if (tens == 8 && ones == 9)
+                        Console.WriteLine("{0}{1}", tens, ones);
+                    else
+                        Console.Write("{0}{1}, ", tens, ones);
+                }
+            }
+        }
+    }
+}
