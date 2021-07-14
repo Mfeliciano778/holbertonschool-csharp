4924fceb7d72003efedae66c1f46d7a613750667
Mfeliciano778 <60366104+Mfeliciano778@users.noreply.github.com>
2021-07-14 14:19:25 -0400

    Initial commit of task 13

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        if (myList.Count < 0)
+            return "None";
+        string maxKey = "";
+        int max = 0;
+
+        foreach (KeyValuePair<String, int> item in myList)
+        {
+            if (item.Value > max)
+            {
+                max = item.Value;
+                maxKey = item.Key;
+            }
+        }
+        return maxKey;
+    }
+}
