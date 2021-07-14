6f39f945b218e0f782b5882f423408d3d29a2b37
Mfeliciano778 <60366104+Mfeliciano778@users.noreply.github.com>
2021-07-14 01:38:56 -0400

    Initial commit of task 0

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size == 0)
+        {
+            Console.WriteLine();
+        } else if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        int[] newArray = new int[size];
+
+        for (int num = 0; num < size; num++)
+        {
+            newArray[num] = num;
+            Console.Write(num < size - 1 ? newArray[num] + " " : newArray[num] + "\n");
+        }
+        return newArray;
+    }
+}
