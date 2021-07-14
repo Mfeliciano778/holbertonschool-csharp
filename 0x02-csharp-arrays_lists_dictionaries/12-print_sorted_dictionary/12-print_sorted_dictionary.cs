476a21aa8abea188f1982d5cc697dfd4bdfd5f4c
Mfeliciano778 <60366104+Mfeliciano778@users.noreply.github.com>
2021-07-14 14:13:54 -0400

    Initial commit of task 12

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        List<String> keySort = new List<string>();
+        int index = 0;
+
+        foreach (string key in myDict.Keys)
+        {
+            keySort.Add(key);
+        }
+        keySort.Sort();
+        foreach (KeyValuePair<String, String> outter in myDict)
+        {
+            foreach (KeyValuePair<String, String> item in myDict)
+            {
+                if (keySort[index] == item.Key)
+                    Console.WriteLine(item.Key + ": " + item.Value);
+            }
+            index++;
+        }
+    }
+}
