a141c9185ae55c497a3224a49840330ed10525f5
Mfeliciano778 <60366104+Mfeliciano778@users.noreply.github.com>
2021-07-14 13:11:53 -0400

    Initial commit of task 5

---
+++ 0x02-csharp-arrays_lists_dictionaries/5-max_integer/5-max_integer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int MaxInteger(List<int> myList)
+    {
+        if (myList.Count > 0)
+        {
+            int max = 0;
+            for (int index = 0; index < myList.Count; index++)
+            {
+                if (myList[index] > max)
+                    max = myList[index];
+            }
+            return max;
+        }
+        Console.WriteLine("List is empty");
+        return -1;
+    }
+}
