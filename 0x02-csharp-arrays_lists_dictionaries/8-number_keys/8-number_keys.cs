05078dcccff7be8e61f151f94f848df6da11ce7c
Mfeliciano778 <60366104+Mfeliciano778@users.noreply.github.com>
2021-07-14 13:30:42 -0400

    Initial commit of task 8

---
+++ 0x02-csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    {
+        int num = 0;
+        foreach (KeyValuePair<string, string> pair in myDict)
+        {
+            num += 1;
+        }
+        return num;
+    }
+}
