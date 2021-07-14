4660308ec757d7e307652c0cdca09099315d4c4e
Mfeliciano778 <60366104+Mfeliciano778@users.noreply.github.com>
2021-07-14 13:18:40 -0400

    Initial commit of task 6

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        List<bool> result = new List<bool>();
+
+        for (int index = 0; index < myList.Count; index++)
+        {
+            if (myList[index] % 2 == 0)
+                result.Add(true);
+            else
+                result.Add(false);
+        }
+        return result;
+    }
+}
