4a5f2fb70e6339f4c84db41fce3595615ffcc548
Mfeliciano778 <60366104+Mfeliciano778@users.noreply.github.com>
2021-07-15 14:39:50 -0400

    Initial commit of task 5

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> newLList = new LinkedList<int>();
+
+        if (size <= 0)
+            return newLList;
+        for (int index = 0; index < size; index++)
+        {
+            newLList.AddLast(index);
+        }
+        foreach (int item in newLList)
+        {
+            Console.WriteLine(item);
+        }
+        return newLList;
+    }
+}
