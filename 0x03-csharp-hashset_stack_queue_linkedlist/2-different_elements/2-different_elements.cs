9c2d608bd0266588fe7b8ab2931268d74bd6bd3d
Mfeliciano778 <60366104+Mfeliciano778@users.noreply.github.com>
2021-07-15 13:52:54 -0400

    Initial commit of task 2

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+    class List
+    {
+        public static List<int> DifferentElements(List<int> list1, List<int> list2)
+        {
+            HashSet<int> firstList = new HashSet<int>(list1);
+            HashSet<int> secondList = new HashSet<int>(list2);
+            firstList.SymmetricExceptWith(secondList);
+
+            List<int> newList = new List<int>();
+            foreach (int item in firstList)
+                newList.Add(item);
+
+            return newList;
+        }
+    }
