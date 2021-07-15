2c260a8f33b19a607bb6ba819ac1286e17226c38
Mfeliciano778 <60366104+Mfeliciano778@users.noreply.github.com>
2021-07-15 13:31:39 -0400

    Initial commit of task 0

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        int sum = 0;
+        var unique = new HashSet<int>(myList);
+        
+        foreach (int item in unique)
+            sum += item;
+        return sum;
+    }
+}
