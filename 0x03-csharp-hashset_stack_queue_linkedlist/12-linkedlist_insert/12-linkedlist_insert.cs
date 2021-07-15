badbd47178abad9e041c7e4129afcb14a181ee3f
Mfeliciano778 <60366104+Mfeliciano778@users.noreply.github.com>
2021-07-15 15:31:14 -0400

    Initial commit of task 12

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> currNode = myLList.First;
+        while (currNode.Next != null)
+        {
+            if (n < currNode.Next.Value)
+            {
+                currNode = myLList.AddAfter(currNode, n);
+                return currNode;
+            }
+            currNode = currNode.Next;
+        }
+        currNode = myLList.AddLast(n);
+        return currNode;
+    }
+}
