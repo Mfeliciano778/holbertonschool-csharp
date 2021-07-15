5eebba5060704eef22c93cd2dd55b945ce27465b
Mfeliciano778 <60366104+Mfeliciano778@users.noreply.github.com>
2021-07-15 15:42:40 -0400

    Intial commit of task 13

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        int idx = 0;
+
+        foreach (int item in myLList)
+        {
+            if (idx == index)
+            {
+                myLList.Remove(item);
+                break;
+            }
+            idx++;
+        }
+    }
+}
