31c3bfbc6b0b6d1525fe9cf0422f2f3ad9df7033
Mfeliciano778 <1652@holbertonschool.com>
2021-07-26 13:37:30 -0400

    Initial commit of task 0

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int len = myList.Count;
+        int index;
+
+        for (index = 0; index < len && index < n; index++)
+        {
+            Console.WriteLine($"{myList[index]}");
+        }
+        return index;
+    }
+}
