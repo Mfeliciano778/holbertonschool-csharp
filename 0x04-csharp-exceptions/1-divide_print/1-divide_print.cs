010d6a7c7d928e92aaf73a815ddb79e972baa066
Mfeliciano778 <1652@holbertonschool.com>
2021-07-26 13:50:23 -0400

    Initial commit of task 1

---
+++ 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Int
+{
+    public static void divide(int a, int b)
+    {
+        int sum = 0;
+        try
+        {
+            sum = a / b;
+        }
+        catch (System.DivideByZeroException)
+        {
+            Console.WriteLine("Cannot divide by zero");
+        }
+        finally
+        {
+            Console.WriteLine($"{a} / {b} = {sum}");
+        }
+    }
+}
