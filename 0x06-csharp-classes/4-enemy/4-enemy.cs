9e7b768a4baa1616047b98ca95aa70a70c4b2631
Mfeliciano778 <1652@holbertonschool.com>
2021-07-27 12:20:39 -0400

    Initial commit of task 4

---
+++ 0x06-csharp-classes/4-enemy/4-enemy.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Enemies
+{
+    class Zombie
+    {
+        public int health;
+
+        private string name = "(No name)";
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+        public Zombie()
+        {
+            health = 0;
+        }
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Health must be greater than or equal to 0");
+            }
+            health = value;
+        }
+
+        public int GetHealth()
+        {
+            return health;
+        }
+    }
+}
