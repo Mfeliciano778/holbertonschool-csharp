c0b1cf84cfb627a3f8d11511221f409b295d259b
Mfeliciano778 <1652@holbertonschool.com>
2021-08-02 21:35:47 -0400

    Initial commit of task 3

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+/// <summary> Class obj </summary>
+class Obj
+{
+    /// <summary> Prints properties and methods </summary>
+    /// <param name="myObj"> The obj we get the properties and methods from </param>
+    public static void Print(object myObj)
+    {
+        Console.WriteLine("{0} Properties:", myObj.GetType().Name);
+        foreach (var prop in myObj.GetType().GetProperties())
+            Console.WriteLine(prop.Name);
+        Console.WriteLine("{0} Methods:", myObj.GetType().Name);
+        foreach (var meth in myObj.GetType().GetMethods())
+            Console.WriteLine(meth.Name);
+    }
+}
