c936d38c7bccaa135c89d46e68880089505e01e6
Mfeliciano778 <1652@holbertonschool.com>
2021-08-02 21:31:54 -0400

    Initial commit of task 0

---
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary> Class Obj </summary>
+class Obj
+{
+    /// <summary> Returns true if an object is an instance of another class </summary>
+    /// <param name="obj"> Object to check </param>
+    /// <returns>true or false </returns>
+    public static bool IsOfTypeInt(object obj)
+    {
+        if (obj.GetType() == typeof(int))
+            return true;
+        return false;
+    }
+}
