12ffd421777c2dee385b95c2abfae7e2714ba91d
Mfeliciano778 <1652@holbertonschool.com>
2021-08-02 21:36:42 -0400

    Initial commit of task 4

---
+++ 0x08-csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary> Empty class Animal </summary>
+class Animal
+{
+}
+
+/// <summary> Empty class Dog </summary>
+class Dog : Animal
+{
+}
