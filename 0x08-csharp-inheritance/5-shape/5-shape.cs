c846db525c7370c17792bd0d7e46f4200090f3f9
Mfeliciano778 <1652@holbertonschool.com>
2021-08-02 21:37:22 -0400

    Initial commit of task 5

---
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+/// <summary> Class Shape throws an exception error with a message.</summary>
+class Shape
+{
+    /// <summary> Throws an exception error with a message.</summary>
+    /// <returns> An exception.</returns>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
