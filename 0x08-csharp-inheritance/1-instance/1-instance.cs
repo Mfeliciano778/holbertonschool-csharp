ab660c01539164a14f72d1dc45e59859039f92b9
Mfeliciano778 <1652@holbertonschool.com>
2021-08-02 21:33:32 -0400

    Initial commit of task 1

---
+++ 0x08-csharp-inheritance/1-instance/1-instance.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary> Class Obj </summary>
+class Obj
+{
+    /// <summary> Returns true if an object is an instance of another class </summary>
+    /// <param name="obj"> Object to check </param>
+    /// <returns>true or false </returns>
+    public static bool IsInstanceOfArray(object obj)
+    {
+        if (obj is Array)
+            return true;
+        return false;
+    }
+}
