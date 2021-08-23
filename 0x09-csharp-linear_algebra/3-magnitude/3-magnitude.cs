cab6703f4de9f29e204e6983bb36a60ce78a2615
Mfeliciano778 <1652@holbertonschool.com>
2021-08-23 16:06:58 -0400

    Initial commit of task 3

---
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class VectorMath
+{
+    public static double Magnitude(double[] vector)
+    {
+        if (vector.GetLength(0) != 2 && vector.GetLength(0) != 3)
+            return (-1);
+        double res = 0.0;
+
+        foreach (int number in vector)
+            res += Math.Pow(number, 2.0);
+        
+        return (Math.Round(Math.Sqrt(res), 2));
+    }
+}
