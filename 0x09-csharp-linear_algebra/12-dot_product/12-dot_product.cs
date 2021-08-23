209a1fadf4205baaf86d6ad23ecf79bc276b86f9
Mfeliciano778 <1652@holbertonschool.com>
2021-08-23 18:35:32 -0400

    Initial comit of task 12

---
+++ 0x09-csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+/// <summary> A class containing a public method called DotProduct </summary>
+class VectorMath
+{
+    /// <summary> To find the dot product of two vectors </summary>
+    /// <param name="vector1">The first vector</param>
+    /// <param name="vector2">The second vector</param>
+    /// <returns>the dot product, else -1</returns>
+    public static double DotProduct(double[] vector1, double[] vector2)
+    {
+        if (vector1.GetLength(0) != 2 && vector1.GetLength(0) != 3)
+            return (-1);
+        if (vector1.GetLength(0) != vector2.GetLength(0))
+            return (-1);
+        double[] sumMatrix = new double[vector1.GetLength(0)];
+        double dot = 0.0;
+
+        for(int index = 0; index < vector1.GetLength(0); index++)
+           sumMatrix[index] = vector1[index] * vector2[index];
+
+        foreach(int num in sumMatrix)
+            dot += num;
+
+        return (dot);
+    }
+}
