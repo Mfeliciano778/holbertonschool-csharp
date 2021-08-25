f6cb176feb7f4ed700c5983e73bfcc95e52afc83
Mfeliciano778 <1652@holbertonschool.com>
2021-08-25 14:27:16 -0400

    Initial commit of task 30

---
+++ 0x09-csharp-linear_algebra/30-cross_product/30-cross_product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+/// <summary> A class containing a public method called CrossProduct </summary>
+class MatrixMath
+{
+    /// <summary> To find the cross product of two vectors </summary>
+    /// <param name="vector1">The first vector</param>
+    /// <param name="vector2">The second vector</param>
+    /// <returns>cross product, else -1</returns>
+    public static double[] CrossProduct(double[] vector1, double[] vector2)
+    {
+        double[] err = { -1 };
+
+        if (vector1.GetLength(0) != 3 || vector2.GetLength(0) != 3)
+            return (err);
+
+        double[] xproduct = {vector1[1] * vector2[2] - vector1[2] * vector2[1],
+                             vector1[2] * vector2[0] - vector1[0] * vector2[2],
+                             vector1[0] * vector2[1] - vector1[1] * vector2[0]
+                            };
+        return (xproduct);
+    }
+}
