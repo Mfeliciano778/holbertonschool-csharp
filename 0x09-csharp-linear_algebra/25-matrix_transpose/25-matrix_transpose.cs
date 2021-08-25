ea6679aba1c5e1f4b8f1d70edebbfde1c87e0571
Mfeliciano778 <1652@holbertonschool.com>
2021-08-25 14:08:55 -0400

    Initial commit of task 25

---
+++ 0x09-csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+/// <summary> A class containing a public method called Transpose </summary>
+class MatrixMath
+{
+    /// <summary> To find the transposed matrix </summary>
+    /// <param name="matrix">The first matrix</param>
+    /// <returns>resulting matrix, else empty matrix</returns>
+    public static double[,] Transpose(double[,] matrix)
+    {
+        double[,] result = {};
+
+        if (matrix == null || matrix.GetLength(0) == 0 || matrix.GetLength(1) == 0)
+            return (result);
+        
+        result = new double[matrix.GetLength(1), matrix.GetLength(0)];
+
+        for (int i = 0; i < matrix.GetLength(0); i++)
+            for (int j = 0; j < matrix.GetLength(1); j++)
+                result[i, j] = matrix[i, j];
+        
+        return (result);
+    }
+}
