85b58603f0c39529f5f43f9e9ba6c3d3e3bdeac7
Mfeliciano778 <1652@holbertonschool.com>
2021-08-25 14:10:34 -0400

    Changed the method of transposing

--- 0x09-csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
+++ 0x09-csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -17,7 +17,7 @@ class MatrixMath
 
         for (int i = 0; i < matrix.GetLength(0); i++)
             for (int j = 0; j < matrix.GetLength(1); j++)
-                result[i, j] = matrix[i, j];
+                result[j, i] = matrix[i, j];
         
         return (result);
     }
