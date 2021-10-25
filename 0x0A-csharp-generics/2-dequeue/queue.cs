85ce0b7292d9eba5617f0be82a47da53d8068524
Mfeliciano778 <1652@holbertonschool.com>
2021-10-25 17:29:32 -0400

    Added XML comments

--- 0x0A-csharp-generics/2-dequeue/queue.cs
+++ 0x0A-csharp-generics/2-dequeue/queue.cs
@@ -1,27 +1,60 @@
 ﻿using System;
 
+/// <summary>
+/// Class for a Queue
+/// </summary>
 class Queue<T>
 {
+    /// <summary>
+    /// Variable that holds the head node
+    /// </summary>
     public Node head;
+    /// <summary>
+    /// Variable that holds the tail node
+    /// </summary>
     public Node tail;
+    /// <summary>
+    /// Variable that holds the number of nodes
+    /// </summary>
     public int count = 0;
 
+    /// <summary>
+    /// Checks the type of the Queue
+    /// </summary>
+    /// <returns> The type of Queue </returns>
     public Type CheckType()
     {
         return typeof(T);
     }
 
+    /// <summary>
+    /// Node class
+    /// </summary>
     public class Node
     {
+        /// <summary>
+        /// Value of the node
+        /// </summary>
         public T value = default(T);
+        /// <summary>
+        /// Variable that holds the next node
+        /// </summary>
         public Node next = null;
 
+        /// <summary>
+        /// Constructor method
+        /// </summary>
+        /// <param name="t"> value we are setting</param>
         public Node(T t)
         {
             value = t;
         }
     }
 
+    /// <summary>
+    /// Creates a node and adds it to the Queue
+    /// </summary>
+    /// <param name="val"> Value in the node </param>
     public void Enqueue(T val)
     {
         Node newNode = new Node(val);
@@ -37,11 +70,19 @@ class Queue<T>
         count++;
     }
 
+    /// <summary>
+    /// Returns the number of nodes
+    /// </summary>
+    /// <returns> Number of nodes </returns>
     public int Count()
     {
         return count;
     }
 
+    /// <summary>
+    /// Dequeue removes the first node and returns the value
+    /// </summary>
+    /// <returns> the value of the first node </returns>
     public T Dequeue()
     {
         if (head == null)
