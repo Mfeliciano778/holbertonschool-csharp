d27aebf160914ea6a3c373bf76fc8703ddd4d0b8
Mfeliciano778 <1652@holbertonschool.com>
2021-10-25 18:18:28 -0400

    Initial commit of task 5

---
+++ 0x0A-csharp-generics/5-concatenate/queue.cs
@@ -0,0 +1,151 @@
+﻿using System;
+
+/// <summary>
+/// Class for a Queue
+/// </summary>
+class Queue<T>
+{
+    /// <summary>
+    /// Variable that holds the head node
+    /// </summary>
+    public Node head;
+    /// <summary>
+    /// Variable that holds the tail node
+    /// </summary>
+    public Node tail;
+    /// <summary>
+    /// Variable that holds the number of nodes
+    /// </summary>
+    public int count = 0;
+
+    /// <summary>
+    /// Checks the type of the Queue
+    /// </summary>
+    /// <returns> The type of Queue </returns>
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+
+    /// <summary>
+    /// Node class
+    /// </summary>
+    public class Node
+    {
+        /// <summary>
+        /// Value of the node
+        /// </summary>
+        public T value = default(T);
+        /// <summary>
+        /// Variable that holds the next node
+        /// </summary>
+        public Node next = null;
+
+        /// <summary>
+        /// Constructor method
+        /// </summary>
+        /// <param name="t"> value we are setting</param>
+        public Node(T t)
+        {
+            value = t;
+        }
+    }
+
+    /// <summary>
+    /// Creates a node and adds it to the Queue
+    /// </summary>
+    /// <param name="val"> Value in the node </param>
+    public void Enqueue(T val)
+    {
+        Node newNode = new Node(val);
+        if (head == null)
+        {
+            head = newNode;
+        } else
+        {
+            tail.next = newNode;
+        }
+        tail = newNode;
+        count++;
+    }
+
+    /// <summary>
+    /// Returns the number of nodes
+    /// </summary>
+    /// <returns> Number of nodes </returns>
+    public int Count()
+    {
+        return count;
+    }
+
+    /// <summary>
+    /// Dequeue removes the first node and returns the value
+    /// </summary>
+    /// <returns> the value of the first node </returns>
+    public T Dequeue()
+    {
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return default(T);
+        }
+
+        T Temp = head.value;
+        head = head.next;
+        count -= 1;
+        return Temp;
+    }
+
+    /// <summary>
+    /// Returns the value of the first node
+    /// </summary>
+    public T Peek()
+    {
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return default(T);
+        }
+        return head.value;
+    }
+
+    public void Print()
+    {
+        Node tmp = head;
+        for (int n = count; n > 0; n--)
+        {
+            System.Console.WriteLine($"{tmp.value}");
+            tmp = tmp.next;
+        }
+        if (count == 0)
+            System.Console.WriteLine("Queue is empty");
+    }
+
+    public string Concatenate()
+    {
+        Node tmp = head;
+        string result = "";
+        
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return null;
+        }
+
+        for (int n = count; n > 0; n--)
+        {
+            if (typeof(T) == typeof(string) || typeof(T) == typeof(char))
+            {
+                result += tmp.value;
+                if (typeof(T) == typeof(string))
+                    result += " ";
+            } else
+            {
+                Console.WriteLine("Concatenate() is for a queue of Strings or Chars only.");
+                return null;
+            }
+            tmp = tmp.next;
+        }
+        return result;
+    }
+}
