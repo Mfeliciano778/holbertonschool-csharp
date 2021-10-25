1f665cb4a9e19b7b86239135d6c1b519e8de52b9
Mfeliciano778 <1652@holbertonschool.com>
2021-10-25 16:11:12 -0400

    Initial commit of task 1

---
+++ 0x0A-csharp-generics/1-enqueue/queue.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Queue<T>
+{
+    public Node head;
+    public Node tail;
+    public int count = 0;
+
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+
+    public class Node
+    {
+        public T value = default(T);
+        public Node next = null;
+
+        public void contructor(T t)
+        {
+            value = t;
+        }
+    }
+
+    public void Enqueue(T value)
+    {
+        Node newNode = new Node();
+        if (count == 0)
+        {
+            head = newNode;
+            newNode.next = tail;
+        } else
+        {
+            tail = newNode;
+            newNode.next = null;
+        }
+        count++;
+    }
+
+    public int Count()
+    {
+        return count;
+    }
+}
