b8ab37796d5c002448aa37f4ea73729002df763a
Mfeliciano778 <1652@holbertonschool.com>
2021-10-25 16:55:12 -0400

    Initial commit of task 2

---
+++ 0x0A-csharp-generics/2-dequeue/queue.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+class Queue<T>
+{
+    public Node head;
+    public Node tail;
+    public int count = 0;
+
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+
+    public class Node
+    {
+        public T value = default(T);
+        public Node next = null;
+
+        public void contructor(T t)
+        {
+            value = t;
+        }
+    }
+
+    public void Enqueue(T value)
+    {
+        Node newNode = new Node();
+        if (count == 0)
+        {
+            head = newNode;
+            newNode.next = tail;
+        } else
+        {
+            tail = newNode;
+            newNode.next = null;
+        }
+        newNode.value = value;
+        count++;
+    }
+
+    public int Count()
+    {
+        return count;
+    }
+
+    public T Dequeue()
+    {
+        T Temp;
+
+        if (count == 0)
+        {
+            System.Console.WriteLine("Queue is empty");
+            return default(T);
+        }
+
+        Temp = head.value;
+        head = head.next;
+        count--;
+        return Temp;
+    }
+}
