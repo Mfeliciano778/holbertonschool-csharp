0db1c8170df0fbbedb22d25b374998feb8f972f6
Mfeliciano778 <1652@holbertonschool.com>
2021-10-27 14:03:59 -0400

    Initial commit of task 0

---
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Player
+{
+    private string name;
+    private float maxHp;
+    private float hp;
+
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        if (maxHp <= 0)
+        {
+            this.maxHp = 100f;
+            this.hp = 100f;
+            System.Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+        }
+        this.maxHp = maxHp;
+        this.hp = maxHp;
+        this.name = name;
+    }
+
+    public void PrintHealth()
+    {
+        System.Console.WriteLine($"{name} has {hp} / {maxHp} health");
+    }
+}
